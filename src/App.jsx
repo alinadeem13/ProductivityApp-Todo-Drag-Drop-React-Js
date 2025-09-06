5fe3bc9d59c8dc6209ded2653af9f8edb346a349
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-09-07 03:39:32 +0500

    setting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import userDarkMode from "./hooks/userDarkMode";
+
 export default function App() {
+  const [darkMode, setDarkMode] = userDarkMode();
+
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-500">
       <h1 className="text-4xl font-bold text-white">Hello Tailwind v4 🚀</h1>
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="px-4 py-2 rounded-lg bg-blue-600 text-white dark:bg-blue-400 dark:text-gray-900 transition-colors"
+      >
+        {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
+      </button>
     </div>
   );
 }
